e80db5b20226bd6553e9d16fc8b7954443c5bb77
Yuhan zhong <715181149@qq.com>
2019-01-16 09:32:15 +0800

    Update config.js

--- duuliy-sys/src/config/config.js
+++ duuliy-sys/src/config/config.js
@@ -1,20 +1,26 @@
 import imgsrc from '@v/images/icon.png'
+// import imgbg from '@v/images/img_bg.png'
 
 module.exports = {
-    siteName: 'AntD Admin',
-    copyright: 'Ant Design Admin  © 2018 zuiidea',
-    logoPath: '/public/images/icon.png',
+    siteName: '云译通后台系统',
+    copyright: '@2017-2018四川译讯信息科技有限公司',
     logoPath: imgsrc,
+//     imgbg:imgbg,
     apiPrefix: '/api/v1',
     fixedHeader: true, // sticky primary layout header
   
     /* Layout configuration, specify which layout to use for route. */
     layouts: [
       {
-        name: 'primary',
+        name: 'login',
         include: [/.*/],
         exlude: [/(\/(en|zh))*\/login/],
       },
+      {
+        name: 'register',
+        include: [/.*/],
+        exlude: [/(\/(en|zh))*\/register/],
+      }
     ],
   
     /* I18n configuration, `languages` and `defaultLanguage` are required currently. */
