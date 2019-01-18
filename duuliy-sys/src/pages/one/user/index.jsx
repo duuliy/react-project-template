793fb7944ce4e622d171724da4334021e6a1ce91
Yuhan zhong <715181149@qq.com>
2019-01-18 10:38:02 +0800

    Update index.jsx

--- duuliy-sys/src/pages/one/user/index.jsx
+++ duuliy-sys/src/pages/one/user/index.jsx
@@ -14,6 +14,7 @@ class user extends Component {
     return (
       <div className="user">
         <div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+<!--           <img src={`/images/peration-type-${fileType}.png`} 动态图片放public的images,静态图片放-->
           <Calendar fullscreen={false} onPanelChange={this.onPanelChange} />
         </div>
       </div>
