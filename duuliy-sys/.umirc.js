900c674d5751cd57e0d7d81b5f08675b388716d5
Yuhan zhong <715181149@qq.com>
2019-02-13 11:14:32 +0800

    Update .umirc.js

--- duuliy-sys/.umirc.js
+++ duuliy-sys/.umirc.js
@@ -1,41 +1,55 @@
 import {resolve} from "path";
 
+
 // ref: https://umijs.org/config/
 export default {
   history: 'hash',
   targets: {
     ie: 11,
   },
-  // publicPath: "./public",
+  publicPath: "./",
   disableCSSModules: true,
   alias: {
     '@': resolve(__dirname, './src'),
-    '@v': resolve(__dirname, './public')
+    '@v': resolve(__dirname, './public'),
+    '@u': resolve(__dirname, './src/utils')
   },
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
       antd: true,
       dva: true,
-      dynamicImport: {
-        loadingComponent: './components/PageLoading/PageLoading',
-      },
+      // dynamicImport: {
+      //   loadingComponent: './components/PageLoading/PageLoading',
+      // },
+      dynamicImport:true,
       title: 'umi-sys',
       dll: false,
-      routes: {
-        include: [/.*/],
-        exlude: [/(\/(en|zh))*\/login/]
-      },
-      hardSource: false,
-      proxy: {
-        "/api": {
-          target: "http://jsonplaceholder.typicode.com/",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api": "/api"
-          }
+      routes: [{
+          include: [/.*/],
+          exlude: [/(\/(en|zh))*\/login/]
+        },
+        {
+          include: [/.*/],
+          exlude: [/(\/(en|zh))*\/register/]
         }
+      ],
+      hardSource: false,
+      locale: {
+        enable: true, // default false
+        default: 'zh-CN', // default zh-CN
+        baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
       },
-    }],
-  ]
+      
+    }]
+  ],
+  proxy: {
+    "/api": {
+      target: "http://localhost:38080",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "/api"
+      }
+    }
+  },
 }
