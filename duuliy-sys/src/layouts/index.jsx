f0386498f4c2f387e7ed588e08251233ffb4bb5e
Yuhan zhong <715181149@qq.com>
2019-02-13 11:24:07 +0800

    Update index.jsx

--- duuliy-sys/src/layouts/index.jsx
+++ duuliy-sys/src/layouts/index.jsx
@@ -5,59 +5,74 @@ import { NavLink } from 'dva/router';
 import BaseLayout from './BaseLayout'
 import { LocaleProvider } from 'antd';
 import PropTypes from 'prop-types'
-// import { I18nProvider } from '@lingui/react'
+import Context from './MenuContext';
+import { ContainerQuery } from 'react-container-query';
+import {validCtr} from '@u/conversion.js'
+import {formatMsg,setLocal,g} from '@u/user.js'
+import cls from 'classnames';
+import {i18n} from '@u/user.js';
 import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import en_US from 'antd/lib/locale-provider/en_US'
 import moment from 'moment';
 
-
-const langs = {
-  'zh_CN': zh_CN,
-  'en_US': en_US,
-}
+const query = {
+  'screen-xs': {
+    maxWidth: 575,
+  },
+  'screen-sm': {
+    minWidth: 576,
+    maxWidth: 767,
+  },
+  'screen-md': {
+    minWidth: 768,
+    maxWidth: 991,
+  },
+  'screen-lg': {
+    minWidth: 992,
+    maxWidth: 1199,
+  },
+  'screen-xl': {
+    minWidth: 1200,
+    maxWidth: 1599,
+  },
+  'screen-xxl': {
+    minWidth: 1600,
+  },
+};
 
 @connect(({ common, loading }) => ({ common, loading }))
 @withRouter
 class Layout extends PureComponent {
   state = {
     catalogs: {},
-    locale: zh_CN
-  };
-  
-  //  change=(lang)=>{
-  //   // this.setState({
-  //   //   language: lang,
-  //   // });
-  // }
 
+  };
 
   render() {
     const {children ,location,language } = this.props;
-    const {locale } = this.state;
-    console.log(language)
-    // const { language } = this.state;
-    if (location.pathname === '/login') {
+    if (location.pathname === '/404'||location.pathname ==='/register') {
       return <Fragment>{ children }</Fragment>
     }
-    // let lang=(language==='zh_CN'?zh_CN:en_US)
 
     return (
-      // <LocaleProvider locale={languages[language]}>
-      <LocaleProvider  locale={locale}>
-        {/* <I18nProvider language={language} catalogs={catalogs}> */}
-          <BaseLayout>{children}</BaseLayout>
-        {/* </I18nProvider> */}
-      </LocaleProvider>
+      <Fragment>
+        <ContainerQuery query={query}>
+          {params => (
+            <Context.Provider>
+              <BaseLayout className={cls(params)}>{children}</BaseLayout>
+            </Context.Provider>
+            )}
+         </ContainerQuery> 
+      </Fragment>
     );
   }
 
+  componentDidMount(){
+    // global.setLocal('zh-CN');
+    global.setLocal(i18n());
+  }
+
   componentDidUpdate(){
-    const {language } = this.props;
-    const {locale } = this.state;
-    console.log(999)
-    this.setState({
-      locale: (!language==='zh_CN'?en_US:zh_CN),
-    });
   }
 }
 
@@ -65,4 +80,4 @@ Layout.propTypes = {
   language:PropTypes.string
 };
 
-export default connect(({common})=>({language:common.language}))(Layout);
+export default connect(({common})=>({language}))(Layout);
