b653ba0799c695fe5d410ec6446986a68a9e57a3
Yuhan zhong <715181149@qq.com>
2019-02-20 10:54:04 +0800

    Update .umirc.js

--- duuliy-sys/.umirc.js
+++ duuliy-sys/.umirc.js
@@ -1,5 +1,4 @@
-import {resolve} from "path";
-
+import {resolve as reso} from "path";
 
 // ref: https://umijs.org/config/
 export default {
@@ -10,10 +9,68 @@ export default {
   publicPath: "./",
   disableCSSModules: true,
   alias: {
-    '@': resolve(__dirname, './src'),
-    '@v': resolve(__dirname, './public'),
-    '@u': resolve(__dirname, './src/utils')
+    '@': reso(__dirname, './src'),
+    '@v': reso(__dirname, './public'),
+    '@u': reso(__dirname, './src/utils')
   },
+  routes: [{
+    path: '/',
+    redirect: '/manage/organization',
+  },
+  {
+    path: '/',
+    component:'../layouts/index.jsx',
+		routes:[
+      {
+        path: '/manage/organiZation',
+        component:'manage/organization/index.jsx',
+      },
+      {
+        path: '/manage/userStati',
+        component:'manage/userStati/index.jsx',
+      },
+      {
+        path: '/manage/rightsManagement',
+        component:'manage/rightsManagement/index.jsx',
+      },
+      {
+        path: '/manage/encryptedDogManagement',
+        component:'manage/encryptedDogManagement/index.jsx',
+      },
+      {
+        path: '/manage/topUpCenter',
+        component:'manage/topUpCenter/index.jsx',
+      },
+
+      {
+        path: '/register',
+        component:'register/index.jsx',
+      },
+
+      {
+        path: '/statireport/corporateTranslationStatistics',
+        component:'statireport/corporateTranslationStatistics/index.jsx',
+      },
+      {
+        path: '/statireport/reswordcount',
+        component:'statireport/reswordcount/index.jsx',
+      },
+      {
+        path: '/statireport/userTranslationDetailStatistics',
+        component:'statireport/userTranslationDetailStatistics/index.jsx',
+      },
+      {
+        path: '/statireport/userTranslationStatistics',
+        component:'statireport/userTranslationStatistics/index.jsx',
+      },
+
+      {
+        path: '/404',
+        component:'404',
+      },
+    ]
+  }
+  ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
@@ -22,7 +79,7 @@ export default {
       // dynamicImport: {
       //   loadingComponent: './components/PageLoading/PageLoading',
       // },
-      dynamicImport:true,
+      dynamicImport: true,
       title: 'umi-sys',
       dll: false,
       routes: [{
@@ -40,7 +97,7 @@ export default {
         default: 'zh-CN', // default zh-CN
         baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
       },
-      
+
     }]
   ],
   proxy: {
