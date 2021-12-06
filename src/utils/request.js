ed210a48c5432dbfba549e6e06ec9c99c3c5b99e
duuliy <715181149@qq.com>
2021-12-06 11:57:42 +0800

    fix: 1

--- src/utils/request.js
+++ src/utils/request.js
@@ -31,14 +31,14 @@ const errPrompt = msg => {
  *
  * @param  {string} method    The type we want to request
  * @param  {string} url       The URL we want to request
- * @param  {object} data      The options we want to pass to "fetch"
- * @return {object}           An object containing either "data" or "err"
+ * @param  {object} payload      The options we want to pass to "fetch"
+ * @return {object}           An object containing either "payload" or "err"
  */
 
-const request = (method, url, data = {}, mock = false) => {
+const request = (method, url, payload = {}, mock = false) => {
   url = (config.mock ? config.mock : mock) ? `/mock${url}` : `/api${url}`
   // 由于ID加密，密文可能存在特殊字符，如"/"，无法用url传参，可以用encodeURIComponent对url中的参数进行编码来避免以上问题
-  const headers = setHeaders(data)
+  const headers = setHeaders(payload)
 
   const loginOutCodes = [
     '401', // 没有权限
@@ -50,13 +50,13 @@ const request = (method, url, data = {}, mock = false) => {
   ]
 
   // 统一处理文件
-  // if (!(data instanceof FormData)) {
-  //   data = JSON.stringify(data)
+  // if (!(payload instanceof FormData)) {
+  //   payload = JSON.stringify(payload)
   // }
 
   if (method === 'get') {
     return new Promise((resolve, reject) => {
-      url = `${url}?${qs.stringify(data)}`
+      url = `${url}?${qs.stringify(payload)}`
       axios.get(baseUrl + url, {
         headers: headers,
       })
@@ -82,7 +82,7 @@ const request = (method, url, data = {}, mock = false) => {
       axios({
         method: method,
         url: baseUrl + url,
-        data: data,
+        data: payload,
         headers: headers,
       })
         .then(checkStatus)
